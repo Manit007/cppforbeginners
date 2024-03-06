ccf640b246f2b8ff6b73606f57c99f7626dcceb8
Manit S Rao <raomanit273@gmail.com>
2024-03-07 00:17:32 +0530

    .
    covered the case where input is either 1 or 0

--- checkprime.cpp
+++ checkprime.cpp
@@ -3,6 +3,11 @@ using namespace std;
 bool checkprime(int n,int *count)
 {
     int i;
+    if(n==1||n==0)
+    {
+        cout<<"Number is neither prime nor composite";
+        exit(0);
+    }
     for(i=2;i<n-1;i++)
     {
         if(n%i==0)
